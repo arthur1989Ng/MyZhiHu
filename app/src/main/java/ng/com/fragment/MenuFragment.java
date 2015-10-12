45f48c7d65e7713750b5dabed6155cc6fb11113b
niangang <360182112@qq.com>
2015-10-12 19:18:16 +0800

    Ng  添加 AppManager

--- app/src/main/java/ng/com/fragment/MenuFragment.java
+++ app/src/main/java/ng/com/fragment/MenuFragment.java
@@ -8,20 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.zip.Inflater;
+
 import ng.com.myzhihu.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MenuFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MenuFragment extends BaseFragment {
 
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+
+
+        return inflater.inflate(R.layout.fragment_menu, container, false);
     }
 }
---
+++ app/src/main/java/ng/com/myzhihu/AppManager.java
@@ -0,0 +1,135 @@
+package ng.com.myzhihu;
+
+import android.app.Activity;
+import android.content.Context;
+
+import java.security.PrivilegedAction;
+import java.util.Stack;
+
+/**
+ * Created by niangang on 2015/10/12.
+ */
+public class AppManager {
+
+    private static Stack<Activity> activityStack;
+    private static AppManager instance;
+
+    //单例模式
+    private AppManager() {
+    }
+
+
+    private static AppManager getInstance() {
+        if (null == instance) {
+
+            instance = new AppManager();
+
+        }
+        return instance;
+    }
+
+    /**
+     * 添加Activity到堆栈
+     */
+    public void addActivity(Activity activity) {
+        if (null == activityStack) {
+            activityStack = new Stack<Activity>();
+        }
+
+        activityStack.add(activity);
+
+    }
+
+    /**
+     * 获取当前Activity（堆栈中最后一个压入的）
+     */
+    public Activity currentActivity() {
+
+        Activity activity = activityStack.lastElement();
+        return activity;
+
+    }
+
+    /**
+     * 结束当前Activity（堆栈中最后一个压入的）
+     */
+    public void finishActivity() {
+
+        Activity activity = activityStack.lastElement();
+
+        finishActivity(activity);
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public void finishActivity(Activity activity) {
+
+        if (activity != null && !activity.isFinishing()) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+                break;
+            }
+        }
+    }
+
+
+    /**
+     * 结束所有Activity
+     */
+    public void finishAllActivity() {
+
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                finishActivity(activityStack.get(i));
+                break;
+            }
+        }
+        activityStack.clear();
+
+    }
+
+    /**
+     * 获取指定的Activity
+     *
+     * @author kymjs
+     */
+    public static Activity getActivity(Class<?> cls) {
+        if (null != activityStack) {
+            for (Activity activity : activityStack) {
+                if (activity.getClass().equals(cls)) {
+                    return activity;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 退出应用程序
+     */
+    public void AppExit(Context context) {
+        try {
+            finishAllActivity();
+            // 杀死该应用进程
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
+        } catch (Exception e) {
+        }
+    }
+
+
+}
